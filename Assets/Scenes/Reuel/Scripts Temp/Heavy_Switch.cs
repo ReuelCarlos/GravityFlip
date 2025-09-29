6e9fc4c9bc722a126a6d66ccbd03c31323f813b4
JhimmyFiggins <carlosreuel24@gmail.com>
2025-09-29 18:49:52 +0800

    Duped some of Ark's File and Push and Pull
    Edited in Reuel Folder:
    Prefabs
    Scripts

--- Assets/Scenes/Reuel/Scripts Temp/Heavy_Switch.cs
+++ Assets/Scenes/Reuel/Scripts Temp/Heavy_Switch.cs
@@ -2,103 +2,81 @@ using UnityEngine;
 
 public class Heavy_Switch : MonoBehaviour
 {
-    
     public GameObject HeavyBox;
-    private GameObject[] hbArray;
-    private bool isSwitchFlipped = false;
+    private Rigidbody2D[] hbArray;
     private int heavyBCount;
-  
 
-     //Interface
+    // Interface
     public Collider2D player;
     private bool playerInRange = false;
+
+    // Lever Sprite Swap
+    public SpriteRenderer leverRenderer; // Assign the lever's SpriteRenderer
+    public Sprite leverOffSprite;        // Lever in "off" position
+    public Sprite leverOnSprite;         // Lever in "on" position
+
+    [Header("Level Settings")]
+    public bool flipped = false; // Checkbox in Inspector
 
     void Start()
     {
         heavyBCount = HeavyBox.transform.childCount;
-     
         hbToArray();
         fConstraint();
+
+        // Apply Inspector checkbox state
+        ApplySwitchState(flipped);
     }
 
-    
     void Update()
     {
-        if(playerInRange && Input.GetKeyDown(KeyCode.E))
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
-           heavyBox();
-        }        
-    }
-
-    void hbToArray(){
-        
-        hbArray = new GameObject[heavyBCount];
-        for(int i = 0; i < heavyBCount; i++) 
-        {
-            hbArray[i] = HeavyBox.transform.GetChild(i).gameObject;
+            flipped = !flipped;
+            ApplySwitchState(flipped);
         }
     }
 
-     //NOT FINAL FOR Box
-    void OnTriggerEnter2D (Collider2D other)
+    void hbToArray()
     {
-
-            playerInRange = true;
+        hbArray = new Rigidbody2D[heavyBCount];
+        for (int i = 0; i < heavyBCount; i++)
+        {
+            hbArray[i] = HeavyBox.transform.GetChild(i).GetComponent<Rigidbody2D>();
+        }
     }
 
-    void OnTriggerExit2D (Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
+        playerInRange = true;
+    }
 
+    void OnTriggerExit2D(Collider2D other)
+    {
         playerInRange = false;
     }
 
-
-    void heavyBox()
+    void ApplySwitchState(bool state)
     {
-       isSwitchFlipped = !isSwitchFlipped; 
-        
+        // Update lever sprite
+        leverRenderer.sprite = state ? leverOnSprite : leverOffSprite;
 
-       if(isSwitchFlipped)
-       {
-            for(int i = 0; i < heavyBCount; i++) 
-            {
-                if(hbArray[i].gameObject.CompareTag("rHeavyBox"))
-                {
-                    hbArray[i].GetComponent<Rigidbody2D>().gravityScale = -1f;
-                    
-                }else if(hbArray[i].gameObject.CompareTag("gHeavyBox"))
-                {
-                    hbArray[i].GetComponent<Rigidbody2D>().gravityScale = -1f;
-                }
-                
-            }
-       
-       }else if(!isSwitchFlipped)
+        // Toggle gravity of heavy boxes
+        float gravity = state ? -1f : 1f;
+        for (int i = 0; i < heavyBCount; i++)
         {
-            for(int i = 0; i < heavyBCount; i++) 
+            if (hbArray[i].CompareTag("rHeavyBox") || hbArray[i].CompareTag("gHeavyBox"))
             {
-                if(hbArray[i].gameObject.CompareTag("rHeavyBox"))
-                {
-                    hbArray[i].GetComponent<Rigidbody2D>().gravityScale = 1f;
-                    
-                }else if(hbArray[i].gameObject.CompareTag("gHeavyBox"))
-                {
-                    hbArray[i].GetComponent<Rigidbody2D>().gravityScale = 1f;
-                }
-                
+                hbArray[i].gravityScale = gravity;
             }
         }
     }
 
     void fConstraint()
     {
-        for(int i = 0; i < heavyBCount; i++) 
-            {
-                
-                hbArray[i].GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePositionX;
-            }
+        for (int i = 0; i < heavyBCount; i++)
+        {
+            hbArray[i].constraints = RigidbodyConstraints2D.FreezePositionX;
+        }
     }
-
-  
-    
 }

4a3f3c84d89c2cfc2f1b82d66960517a30701d9d
JhimmyFiggins <146190916+JhimmyFiggins@users.noreply.github.com>
2025-10-21 03:40:20 +0800

    Revert "Tutorial semi final"
    This reverts commit 90f83679fe71c062bbe20943f877824ff64ea8af.

--- Assets/Scenes/Reuel/Scripts Temp/TutorialPanels.cs
+++ Assets/Scenes/Reuel/Scripts Temp/TutorialPanels.cs
@@ -1,67 +1,25 @@
-using TMPro;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+
 public class TutorialPanels : MonoBehaviour
 {
-    public GameObject panel;
-    
+    public GameObject Panel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {   
-        StartCoroutine(DelayOnStart());    
+    {
+        Panel.SetActive(false);
     }
-
-    
-    public void pauseOnStart(){
-
-        if(this.gameObject.CompareTag("Intro")){
-            Debug.Log("this is intro");
-            Time.timeScale = 0f;
-        }
-    }
-    public void exitButton(){
-        Time.timeScale = 1f;
-    }
-
 
     void OnCollisionEnter2D( Collision2D other){
 
-        if(other.gameObject.CompareTag("Player") && this.gameObject.CompareTag("gLightBox") ){
-            panel.SetActive(true);
-            Time.timeScale = 0f;
-
-        
-        }else if(other.gameObject.CompareTag("Player") && this.gameObject.CompareTag("rLightBox")){
-            panel.SetActive(true);
-            Time.timeScale = 0f;
-        }else if(other.gameObject.CompareTag("Player") && this.gameObject.CompareTag("PressureCollision")){
-            Debug.Log("time start");
-            panel.SetActive(true);
-            Time.timeScale = 0f;
+        if(other.gameObject.CompareTag("Player")){
+            Panel.SetActive(true);
         }
     }
 
-    void OnTriggerEnter2D( Collider2D other){
-        Debug.Log("Trigger");
-        //Time Panel
-        if(other.gameObject.CompareTag("rLightBox") && this.gameObject.CompareTag("RedPlate")){
-            panel.SetActive(true);
-            Time.timeScale = 0f;
-        }else if(other.gameObject.CompareTag("Player") && this.gameObject.CompareTag("HazardTrigger")){
-            panel.SetActive(true);
-            Time.timeScale = 0f;
-        }
-    }
-    
-    IEnumerator DelayOnStart(){
-        int i = 0;
-        while(i==0){
-            yield return new WaitForSeconds(1f);
-            pauseOnStart();
-            i++;
-        }
-    }
+    void OnCollisionExit2D( Collision2D other){
 
-    
+        if(other.gameObject.CompareTag("Player")){
+            Panel.SetActive(false);
+        }
+    }
 }

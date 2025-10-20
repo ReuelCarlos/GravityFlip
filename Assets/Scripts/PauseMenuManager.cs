d267bdf0084c6e470c94c51f5b294975e158d6cd
Acrophyllix <acrophyllix@gmail.com>
2025-10-21 01:08:03 +0800

    test

--- Assets/Scripts/PauseMenuManager.cs
+++ Assets/Scripts/PauseMenuManager.cs
@@ -47,7 +47,7 @@ public class PauseMenuManager : MonoBehaviour
     public void ExitToMainMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("MainMenu");
+        SceneManager.LoadScene("MainMenu1");
     }
 
     private void PauseGame()
